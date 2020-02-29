36d57b2bd4a23363a2b8af7482adb59b6ef9ab9b
Issaadi Sami <samiisd@hotmail.fr>
2020-02-29 14:51:22 +0100

    Convert  module to header only module

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -7,10 +7,29 @@
 
 class Camera {
 public:
-  Camera(float imHeight, float imWidth, Vec3 eye, Vec3 focus, Vec3 up,
-         float focal, float fov, float aspectRatio);
+  Camera(float imHeight, float imWidth, const Vec3 eye, const Vec3 focus,
+         Vec3 up, float focal, float fov, float aspectRatio) {
+    eye_ = eye;
 
-  [[nodiscard]] Vec3 pixelToWorld(float y, float x) const;
+    up_ = up.normalized();
+    forward_ = (focus - eye).normalized();
+    left_ = forward_.cross(up_);
+
+    // convert degree FOV in radians
+    fov = fov * static_cast<float>(M_PI / 180.0);
+
+    float H = 2.0f * focal * std::tan(fov / 2.0f);
+    float W = H * aspectRatio;
+
+    C_ = eye + focal * forward_;
+    L_ = C_ + (up_ * 0.5f * H) + (left_ * 0.5f * W);
+
+    scaleY_ = H / imHeight;
+    scaleX_ = W / imWidth;
+  }
+  [[nodiscard]] inline Vec3 pixelToWorld(float y, float x) const {
+    return L_ - ((y * scaleY_) * up_ + (x * scaleX_) * left_);
+  }
 
   [[nodiscard]] const Vec3 &eye() const { return eye_; };
 
